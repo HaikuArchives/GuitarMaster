66c3520ec36a4544c310617e042e414fdcae662d
quiqueiii <quiqueiii@09e4c2fc-fc80-11dd-9fdb-3debd5008f28>
2009-03-06 22:00:16 +0000

    Included MidiReader class. Hook itemlist song clicks

---
+++ trunk/TMidiReader.h
@@ -0,0 +1,28 @@
+#ifndef TMIDIREADER_H
+#define TMIDIREADER_H
+
+
+namespace org
+{
+	namespace toxic
+	{
+		namespace resources
+		{
+			class TMidiReader
+			{
+				public:
+				
+				TMidiReader();
+				~TMidiReader();
+				
+				void Close();
+				
+				
+			};
+		}
+		
+	}	
+}
+
+
+#endif
